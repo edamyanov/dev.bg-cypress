e6f9e739debdc156c1e694a6c4433a39c193dde0
Emanuil Damyanov <e.damyanov@ocado.com>
2021-08-23 14:03:02 +0300

    Add go to events test

--- cypress/integration/dev.bg/register.spec.js
+++ cypress/integration/dev.bg/register.spec.js
@@ -1,5 +1,12 @@
 describe('My First Cypress Test', function() {
     it('Visits dev.bg', ()=> {
         cy.visit("https://dev.bg/");
+        cy.contains("Job Board за IT общността");
     })
+
+    it('Open events', ()=> {
+        cy.visit("https://dev.bg/");
+        cy.contains("Събития").click();
+    })
+
 })
