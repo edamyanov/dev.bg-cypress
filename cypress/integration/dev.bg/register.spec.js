68cac2ff0067c20535bd5a1177901c16ce7b4d18
Emanuil Damyanov <e.damyanov@ocado.com>
2021-08-23 14:03:38 +0300

    Add test for registering

--- cypress/integration/dev.bg/register.spec.js
+++ cypress/integration/dev.bg/register.spec.js
@@ -9,4 +9,11 @@ describe('My First Cypress Test', function() {
         cy.contains("Събития").click();
     })
 
+    it('Go to event and register for it', ()=> {
+        cy.visit("https://dev.bg/");
+        cy.contains("Разрешавам всички").click();
+        cy.contains("Събития").click();
+        cy.get('.section-inner a[href$="testove/"]').first().click();
+        cy.contains("Регистрирай се").click();
+    })
 })
